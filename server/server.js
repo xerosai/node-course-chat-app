7daf4002ddc84baef2b21bdde2006b4db0b35290
Simon Neufville <simon@xrscodeworks.com>
2018-05-14 14:44:09 -0500

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const publicPath = path.join(__dirname, '../public');
+
+const app = express();
+
+app.use(express.static(publicPath));
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html');
+})
+
+const PORT = process.env.NODE_ENV || 3455;
+app.listen(PORT, () => {
+    console.log(`Started server on PORT: ${PORT}`);
+});
